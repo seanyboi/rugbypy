72a38c64401199ad8d01f195e05b2735750964bf
seanyboi <oconnorjamessean@gmail.com>
2023-04-02 20:01:36 +0100

    Updated README

--- rugbypy/_modidx.py
+++ rugbypy/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://seanyboi.github.io',
                 'git_url': 'https://github.com/seanyboi/rugbypy',
                 'lib_path': 'rugbypy'},
-  'syms': {'rugbypy.core': {'rugbypy.core.foo': ('core.html#foo', 'rugbypy/core.py')}}}
+  'syms': { 'rugbypy.core': {'rugbypy.core.foo': ('fetch_match_stats.html#foo', 'rugbypy/core.py')},
+            'rugbypy.fetch_match_stats': { 'rugbypy.fetch_match_stats.say_hello': ( 'fetch_match_stats.html#say_hello',
+                                                                                    'rugbypy/fetch_match_stats.py')}}}
